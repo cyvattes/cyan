9faa988993de8d031e9e25a79550cd3752f50ce6
Cyrus Vattes <c.vattes@samsung.com>
2021-12-15 16:28:51 -0800

    Add PWA homepage

---
+++ cyan_pwa/src/app.rs
@@ -0,0 +1,43 @@
+use eframe::egui::{Color32, CtxRef, Direction, FontDefinitions, FontFamily, Layout, Separator, TextStyle, Ui, Vec2};
+
+const WHITE: Color32 = Color32::from_rgb(255, 255, 255);
+
+pub struct PWA {
+    pub(crate) size: Vec2,
+}
+
+impl PWA {
+    pub fn new() -> PWA {
+        PWA {
+            size: Vec2::new(800., 600.),
+        }
+    }
+
+    pub fn configure_font(&self, ctx: &CtxRef) {
+        let mut fonts = FontDefinitions::default();
+        fonts.family_and_size.insert(
+            TextStyle::Heading,
+            (FontFamily::Proportional, 35.),
+        );
+        fonts.family_and_size.insert(
+            TextStyle::Body,
+            (FontFamily::Proportional, 20.),
+        );
+        ctx.set_fonts(fonts);
+    }
+
+    pub fn configure_size(&mut self, ctx: &CtxRef) {
+        self.size = Vec2::new(
+            ctx.input().screen_rect().width(),
+            ctx.input().screen_rect().height(),
+        );
+    }
+
+    pub fn render_title(&self, ui: &mut Ui) {
+        let title: String = String::from("Cyan Text Abstraction Tool");
+        ui.with_layout(Layout::centered_and_justified(Direction::LeftToRight), |ui| {
+            ui.colored_label(WHITE, title);
+        });
+        ui.add(Separator::default());
+    }
+}
--- cyan_pwa/src/lib.rs
+++ cyan_pwa/src/lib.rs
@@ -1,8 +1,49 @@
+mod app;
+pub use app::PWA;
+
+use eframe::egui::{
+    CentralPanel,
+    CtxRef,
+    ScrollArea,
+    Vec2,
+};
+
+use eframe::epi::{
+    App,
+    Frame,
+    Storage,
+};
+
 #[cfg(target_arch = "wasm32")]
 use eframe::wasm_bindgen::{self, prelude::*};
 
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen]
-pub fn run() {
+pub fn run(canvas_id: &str) {
+    let pwa = PWA::new();
+    let window = Box::new(pwa);
+    eframe::start_web(canvas_id, window).unwrap();
+}
 
+impl App for PWA {
+    fn update(&mut self, ctx: &CtxRef, _frame: &mut Frame<'_>) {
+        CentralPanel::default().show(ctx, |ui| {
+            ScrollArea::auto_sized().show(ui, |ui| {
+                self.render_title(ui);
+            });
+        });
+    }
+
+    fn setup(&mut self, ctx: &CtxRef, _frame: &mut Frame<'_>, _storage: Option<&dyn Storage>) {
+        self.configure_font(ctx);
+        self.configure_size(ctx);
+    }
+
+    fn name(&self) -> &str {
+        "PWA"
+    }
+
+    fn max_size_points(&self) -> Vec2 {
+        self.size
+    }
 }
