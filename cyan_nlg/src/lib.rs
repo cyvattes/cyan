9d3df590668fe4d5143ac32e5930899b51dff94b
Cyrus Vattes <c.vattes@samsung.com>
2021-12-30 23:02:36 -0800

    Add debug code for R-length rouge summarization

--- cyan_nlg/src/lib.rs
+++ cyan_nlg/src/lib.rs
@@ -3,10 +3,44 @@ pub mod ngram;
 mod summarizer;
 mod tokenizer;
 
+use futures::future::try_join_all;
 use ngram::NGram;
-use tokio;
+use tokio::{
+    self,
+    // try_join,
+    task::JoinHandle,
+    time::Instant,
+};
+
+fn spawn(s: &str) -> JoinHandle<String> {
+    let cp = s.to_string();
+    tokio::task::spawn_blocking(move || {
+        summarizer::summarize(&cp)
+    })
+}
 
 pub async fn summarize(text: &str) -> String {
+    // TODO: Summarizer is sharing resources, forcing
+    //  threads to be run concurrently, but not
+    //  in parallel. Need to see if we can fix this.
+    // const R: usize = 3;
+    // let mut handles = Vec::with_capacity(R);
+    //
+    // let start = Instant::now();
+    // for _ in 0..R {
+    //     handles.push(spawn(text))
+    // }
+    // let v = match try_join_all(handles).await {
+    //     Ok(val) => val,
+    //     Err(_) => vec![
+    //         String::new(),
+    //         String::new(),
+    //         String::new(),
+    //     ],
+    // };
+    // println!("{:?}\n{:?}\n{:?}\n{:?}", start.elapsed(), v[0], v[1], v[2]);
+    // v[0].to_string()
+
     let t = text.to_string();
     tokio::task::spawn_blocking(move || {
         summarizer::summarize(&t)
--- cyan_nlg/src/summarizer.rs
+++ cyan_nlg/src/summarizer.rs
@@ -1,10 +1,16 @@
 use rust_bert::pipelines::summarization::SummarizationModel;
+use std::time::Instant;
+use std::thread;
 
 pub fn summarize(text: &str) -> String {
-    SummarizationModel::new(
+    let t = Instant::now();
+    println!("Thread {:?} started: {:?}", thread::current().id(), t);
+    let res = SummarizationModel::new(
         Default::default(),
     )
         .unwrap()
         .summarize([text])
-        .join(" ")
+        .join(" ");
+    println!("Thread {:?} took: {:?}", thread::current().id(), t.elapsed());
+    res
 }
