b18175198581d9389d68d75a6e36c4cf3db85a36
Cyrus Vattes <c.vattes@samsung.com>
2022-01-08 01:03:33 -0800

    Update histograms with n_gram labels

--- cyan_vis/src/lib.rs
+++ cyan_vis/src/lib.rs
@@ -1,8 +1,14 @@
 mod ngram;
 
+use std::any::Any;
 use futures::join;
+use plotters::chart::SeriesAnno;
+use plotters::coord::types::RangedSlice;
 use plotters::prelude::*;
+use plotters::prelude::SegmentValue::CenterOf;
 use tokio::task::spawn_blocking;
+
+const NGRAM_FIELD_LENGTH: usize = 10;
 
 pub async fn plot(src: &Vec<String>, abs: &Vec<String>) {
     join!(
@@ -15,12 +21,9 @@ pub async fn plot(src: &Vec<String>, abs: &Vec<String>) {
 async fn plot_n_grams(name: &'static str, text: &Vec<String>) {
     let t = text.to_owned();
     spawn_blocking(move || {
-        // 1: calculate top X n-grams for text
-        let top = ngram::get_top(t, 10);
+        let top = ngram::get_top(t, NGRAM_FIELD_LENGTH);
+        plot_histogram(name, &top);
         println!("{:?}", top);
-
-        // 2: graph
-        plot_histogram(name, &top)
     })
         .await
         .unwrap();
@@ -40,12 +43,15 @@ fn plot_histogram(name: &'static str, data: &Vec<(String, usize)>) {
     let root = BitMapBackend::new(file, (288, 288)).into_drawing_area();
     root.fill(&WHITE).unwrap();
 
+    // let fields: Box<[String]> = Box::from(data.iter().map(|(field, _)| field).collect());
+    // let b =(1u32..10u32).into_segmented();
+    // println!("{:?}", b);
     let mut chart = ChartBuilder::on(&root)
         .x_label_area_size(35)
         .y_label_area_size(40)
         .margin(5)
         .build_cartesian_2d(
-            (1u32..10u32).into_segmented(),
+            (1u32..NGRAM_FIELD_LENGTH as u32).into_segmented(),
             0u32..data[0].1 as u32 + 2,
         )
         .unwrap();
@@ -56,6 +62,13 @@ fn plot_histogram(name: &'static str, data: &Vec<(String, usize)>) {
         .bold_line_style(&WHITE.mix(0.3))
         .y_desc("Count")
         .x_desc("N-Gram")
+        .x_label_formatter(&|sv| {
+            let x: usize = match sv {
+                CenterOf(val) => *val,
+                _ => 0,
+            } as usize;
+            format!("{:?}", data[x-1].0)
+        })
         .axis_desc_style(("sans-serif", 12))
         .draw()
         .unwrap();
@@ -73,5 +86,5 @@ fn plot_histogram(name: &'static str, data: &Vec<(String, usize)>) {
             )
     ).unwrap();
 
-    root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
+    root.present().expect("Directory not found");
 }
